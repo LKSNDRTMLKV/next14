38022b1837748ed4c16f355035a4ca97724fef07
LKSNDRTMLKV <aleksandar.temelkov@live.com>
2023-10-29 21:37:31 +0100

    first chapters..

---
+++ app/ui/fonts.ts
@@ -0,0 +1,13 @@
+import { Inter, Lusitana, Cormorant_Garamond } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+
+export const lusitana = Lusitana({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
+
+export const cormorant = Cormorant_Garamond({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
